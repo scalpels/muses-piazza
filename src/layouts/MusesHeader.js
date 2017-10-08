d174be4bd2f570449ad2230144221e4f3a3a4094
nonumber1989 <nonumber1989@gmail.com>
2017-10-08 11:17:11 +0800

    add fetch

--- src/layouts/MusesHeader.js
+++ src/layouts/MusesHeader.js
@@ -16,6 +16,7 @@ const MusesHeader = () => (
         <Menu.Item key="1"><Link to='/'>Home</Link></Menu.Item>
         <Menu.Item key="2"><Link to='/todo'>todo</Link></Menu.Item>
         <Menu.Item key="3"><Link to='/footer'>footer</Link></Menu.Item>
+        <Menu.Item key="4"><Link to='/itemList'>itemList</Link></Menu.Item>
       </Menu>
     </Header>
 	)  
